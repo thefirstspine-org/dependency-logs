c539b3791420353c207b2ba4660c7e23b5a7b176
TheFirstSpine <teddy@thefirstspine.fr>
2023-10-04 01:32:24 +0530

    Added tests

---
+++ src/logs.service.spec.ts
@@ -0,0 +1,43 @@
+import {describe, expect, beforeEach, it} from '@jest/globals';
+import { LogsService } from './logs.service';
+
+describe('AuthService', () => {
+  let service;
+
+  beforeEach(async () => {
+    service = new LogsService();
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should log to console with level "info"', async () => {
+    const consoleSpy = jest.spyOn(console._stdout, 'write');
+    service.info("test");
+    await new Promise((r) => setTimeout(r, 2000));
+    expect(consoleSpy).toHaveBeenCalled();
+  });
+
+  it('should log to console with level "warn"', async () => {
+    const consoleSpy = jest.spyOn(console._stdout, 'write');
+    service.warning("test");
+    await new Promise((r) => setTimeout(r, 2000));
+    expect(consoleSpy).toHaveBeenCalled();
+  });
+
+  it('should log to console with level "error"', async () => {
+    const consoleSpy = jest.spyOn(console._stdout, 'write');
+    service.error("test");
+    await new Promise((r) => setTimeout(r, 2000));
+    expect(consoleSpy).toHaveBeenCalled();
+  });
+});
+
+declare global {
+  interface Console {
+    _stdout: {
+      write: (payload: any) => void,
+    },
+  }
+}
